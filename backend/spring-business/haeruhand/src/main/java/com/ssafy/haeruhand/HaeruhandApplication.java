774ca11cb705629b908c09f916025ca0d90ee4ce
mindyso7151 <mindyso7151@gmail.com>
2025-07-30 11:09:00 +0900

    chore: haeruhand 프로젝트 생성

---
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/HaeruhandApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.haeruhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeruhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeruhandApplication.class, args);
+	}
+
+}
