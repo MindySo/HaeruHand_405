afe7c32cd766a63567387303339536461ac180a1
mindyso7151 <mindyso7151@gmail.com>
2025-07-31 18:05:05 +0900

    feat: 카카오 OAuth 로그인 화면 구현 (S13P11A405-47)

---
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/domain/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.ssafy.haeruhand.domain.user.controller;
+
+import com.ssafy.haeruhand.domain.user.service.OAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final OAuthService oAuthService;
+
+    @GetMapping("/login/success")
+    public String loginSuccess(OAuth2AuthenticationToken token) {
+        oAuthService.processOAuthPostLogin(token);
+        return "로그인 성공!";
+    }
+}
---
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/domain/user/entity/User.java
@@ -0,0 +1,22 @@
+package com.ssafy.haeruhand.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String kakaoSub;
+    private String email;
+    private String nickname;
+    private String profileImage;
+    private String provider;
+}
