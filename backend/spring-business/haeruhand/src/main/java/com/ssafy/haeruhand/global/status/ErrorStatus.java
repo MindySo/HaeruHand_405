77441e7844cee69a0fb642c626b0bcbc74c29d58
mindyso7151 <mindyso7151@gmail.com>
2025-08-06 02:52:18 +0900

    style: 세미콜론 수정 (S13P11A405-61)

--- backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/global/status/ErrorStatus.java
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/global/status/ErrorStatus.java
@@ -35,7 +35,7 @@ public enum ErrorStatus implements BaseErrorStatus {
     FCM_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, 404, "등록되지 않은 FCM 토큰입니다."),
     FCM_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 500, "FCM 알림 전송에 실패했습니다."),
     FCM_TOKEN_DUPLICATE(HttpStatus.CONFLICT, 409, "이미 등록된 FCM 토큰입니다."),
-    INVALID_DEVICE_TYPE(HttpStatus.BAD_REQUEST, 400, "지원하지 않는 디바이스 타입입니다.");
+    INVALID_DEVICE_TYPE(HttpStatus.BAD_REQUEST, 400, "지원하지 않는 디바이스 타입입니다."),
 
     // s3
     UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST, 400, "지원하지 않는 이미지 업로드 타입입니다.");
