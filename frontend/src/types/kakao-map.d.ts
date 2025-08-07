58e51163bc93c0b8996173ec6607111bb5200847
leekyumin <leekyumin0901@gmail.com>
2025-08-07 12:16:36 +0900

    feat: BuddyTrackingPage 구현 (S13P11A405-108)

---
+++ frontend/src/types/kakao-map.d.ts
@@ -0,0 +1,47 @@
+declare global {
+  interface Window {
+    kakao: {
+      maps: {
+        load: (callback: () => void) => void;
+        Map: new (container: HTMLElement, options: MapOptions) => Map;
+        LatLng: new (lat: number, lng: number) => LatLng;
+        Marker: new (options: MarkerOptions) => Marker;
+        MarkerImage: new (src: string, size: Size) => MarkerImage;
+        Size: new (width: number, height: number) => Size;
+      };
+    };
+  }
+}
+
+interface MapOptions {
+  center: LatLng;
+  level: number;
+}
+
+interface MarkerOptions {
+  map: Map;
+  position: LatLng;
+  image?: MarkerImage;
+}
+
+interface Map {
+  // Map 인스턴스의 메서드들
+}
+
+interface LatLng {
+  // LatLng 인스턴스의 메서드들
+}
+
+interface Marker {
+  // Marker 인스턴스의 메서드들
+}
+
+interface MarkerImage {
+  // MarkerImage 인스턴스의 메서드들
+}
+
+interface Size {
+  // Size 인스턴스의 메서드들
+}
+
+export {};
