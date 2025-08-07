5480a5af9df4a1f481db140cb86596e5c2275734
joyerimi <chosarah70@gmail.com>
2025-08-08 01:26:24 +0900

    feat: 날씨 정보 엔티티 ì설정

---
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/domain/weather/entity/FisheryWeather.java
@@ -0,0 +1,52 @@
+package com.ssafy.haeruhand.domain.weather.entity;
+
+import com.ssafy.haeruhand.domain.fishery.entity.Fishery;
+import com.ssafy.haeruhand.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "fishery_weather")
+@Entity
+public class FisheryWeather extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fishery_weather_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fishery_id", nullable = false)
+    private Fishery fishery;
+
+    @Column(name = "forecast_date", nullable = false)
+    private LocalDate forecastDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "forecast_time_period", length = 10, nullable = false)
+    private ForecastTimePeriod forecastTimePeriod;
+
+    @Column(name = "average_air_temperature", precision = 4, scale = 1)
+    private BigDecimal averageAirTemperature;
+
+    @Column(name = "average_water_temperature", precision = 4, scale = 1)
+    private BigDecimal averageWaterTemperature;
+
+    @Column(name = "average_wave_height", precision = 4, scale = 1)
+    private BigDecimal averageWaveHeight;
+
+    @Column(name = "average_wind_speed", precision = 4, scale = 1)
+    private BigDecimal averageWindSpeed;
+
+    @Column(name = "weather_description", length = 30)
+    private String weatherDescription;
+
+    @Column(name = "sea_travel_index", length = 10)
+    private String seaTravelIndex;
+}
