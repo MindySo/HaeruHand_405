2db968e56d6de0c9f176e28a965f85422c991b54
joyerimi <chosarah70@gmail.com>
2025-08-07 10:41:16 +0900

    fix: api 경로 제거

--- backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/domain/notification/controller/FCMController.java
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/domain/notification/controller/FCMController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/notifications/")
+@RequestMapping("/v1/notifications/")
 @RequiredArgsConstructor
 @Tag(name = "FCM Test", description = "FCM 알림 테스트 API")
 public class FCMController {
