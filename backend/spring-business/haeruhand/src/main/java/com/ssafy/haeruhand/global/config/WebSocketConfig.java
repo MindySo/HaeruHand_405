88fba12a7c24d56167a3349509d1b7ddea5efbc5
오일우 <ooo952@naver.com>
2025-08-07 17:31:56 +0900

    feat: 웹소켓 핸드셰이크 검증을 STOMP 통신으로 통합 (S13P11A405-116)

--- backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/global/config/WebSocketConfig.java
+++ backend/spring-business/haeruhand/src/main/java/com/ssafy/haeruhand/global/config/WebSocketConfig.java
@@ -1,8 +1,9 @@
 package com.ssafy.haeruhand.global.config;
 
-import com.ssafy.haeruhand.global.websocket.WsHandshakeInterceptor;
+import com.ssafy.haeruhand.global.websocket.StompAuthChannelInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,14 +14,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WsHandshakeInterceptor wsHandshakeInterceptor;
+    private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .addInterceptors(wsHandshakeInterceptor)
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+        registry.addEndpoint("/v1/ws")
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
@@ -28,5 +27,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableSimpleBroker("/sub");
         registry.setUserDestinationPrefix("/user");
+    }
+    
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        // STOMP 메시지 인터셉터 등록 (CONNECT 시점에서 인증)
+        registration.interceptors(stompAuthChannelInterceptor);
     }
 }
